de0e61b4ad5da02f11610fe7c36dca0d67f01487
capt-nemo429 <nemo429@protonmail.com>
2022-07-06 21:20:22 -0300

    complete types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,92 @@
+export type BoxId = string;
+export type TransactionId = string;
+export type HexString = string;
+export type ErgoTree = string;
+export type TokenId = string;
+
+export type TokenAmount = {
+  tokenId: TokenId;
+  amount: string;
+  name?: string;
+  decimals?: number;
+};
+
+export type NonMandatoryRegisters = Partial<{
+  R4: HexString;
+  R5: HexString;
+  R6: HexString;
+  R7: HexString;
+  R8: HexString;
+  R9: HexString;
+}>;
+
+export type BoxCandidate = {
+  boxId?: BoxId;
+  ergoTree: ErgoTree;
+  creationHeight: number;
+  value: string;
+  assets: TokenAmount[];
+  additionalRegisters: NonMandatoryRegisters;
+};
+
+export type Box = {
+  boxId: BoxId;
+  transactionId: TransactionId;
+  index: number;
+  ergoTree: ErgoTree;
+  creationHeight: number;
+  value: string;
+  assets: TokenAmount[];
+  additionalRegisters: NonMandatoryRegisters;
+  confirmed?: boolean;
+};
+
+export type ContextExtension =
+  | {
+      values: { [key: string]: string };
+    }
+  | {};
+
+export type UnsignedInput = {
+  boxId: BoxId;
+  transactionId: TransactionId;
+  index: number;
+  ergoTree: ErgoTree;
+  creationHeight: number;
+  value: string;
+  assets: TokenAmount[];
+  additionalRegisters: NonMandatoryRegisters;
+  extension: ContextExtension;
+};
+
+export type DataInput = {
+  boxId: BoxId;
+};
+
+export type UnsignedTransaction = {
+  id?: string;
+  inputs: UnsignedInput[];
+  dataInputs: DataInput[] | UnsignedInput[];
+  outputs: BoxCandidate[];
+};
+
+export type ProverResult = {
+  proofBytes: HexString;
+  extension: ContextExtension;
+};
+
+export type SignedInput = Readonly<{
+  boxId: BoxId;
+  spendingProof: ProverResult;
+}>;
+
+export type SignedTransaction = Readonly<{
+  id: TransactionId;
+  inputs: SignedInput[];
+  dataInputs: DataInput[];
+  outputs: Box[];
+}>;
+
 type EIP12ConnectionOptions = {
   /**
    * Specify if the wallet should instantiate a global `ergo` const with
@@ -36,14 +125,14 @@ export interface EIP12Connection {
 }
 
 export interface EIP12ErgoAPI {
-  get_utxos(amount?: string, tokenId?: string): Promise<any[]>;
+  get_utxos(amount?: string, tokenId?: string): Promise<Box[]>;
   get_balance(tokenId?: string): Promise<string>;
   get_used_addresses(): Promise<string[]>;
   get_unused_addresses(): Promise<string[]>;
   get_change_address(): Promise<string>;
-  sign_tx(transaction: any): Promise<any>;
-  sign_data(address: string, message: string): Promise<any>;
-  submit_tx(transaction: any): Promise<any>;
+  sign_tx(transaction: UnsignedTransaction): Promise<SignedTransaction>;
+  sign_data(address: string, message: string): Promise<unknown>;
+  submit_tx(transaction: SignedTransaction): Promise<string>;
 }
 
 declare global {
